e49ba050f886499bfee4431591a6de6885fb5002
Vyacheslav <pandaemoneon86@gmail.com>
2024-06-30 20:58:34 +0200

    few edits

--- src/main/java/Lession_7/Task_1/Bowl.java
+++ src/main/java/Lession_7/Task_1/Bowl.java
@@ -2,6 +2,7 @@ package Lession_7.Task_1;
 
 // для удобстава делаем отдельный класс Еда
 public class Bowl {
+    // ИСПРАВЛЕНО: лишнее поле удалено
     protected int food = 0;
     // ИСПРАВЛЕНО: теперь еда закидывается при создании объекта
     public Bowl(int food) {
