7f087af1d15db216938545d16ef548e2c8b8b390
Vyacheslav <pandaemoneon86@gmail.com>
2024-07-08 15:37:48 +0200

    create test NG Factorial

---
+++ src/main/java/Lesson_10/CalcFactorial.java
@@ -0,0 +1,20 @@
+package Lesson_10;
+
+import java.math.BigInteger;
+
+public class CalcFactorial {
+    public static BigInteger factorial(int n) {
+        BigInteger fact = BigInteger.ONE;
+        if (n < 0) {
+            throw new ArithmeticException();
+        }
+        try {
+            for (int i = 1; i <= n; i++) {
+                fact = fact.multiply(BigInteger.valueOf(i));
+            }
+            return fact;
+        } catch (ArithmeticException e) {
+            return fact;
+        }
+    }
+}
